a2ad73bc3a9ca590a7e9e876277567dcf3696fdf
robiullah1971 <126934911+robiullah1971@users.noreply.github.com>
2023-03-11 11:02:28 +0600

    functions

---
+++ 5_functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Functions 
+1. Name   ---- required
+2. Paramters -- Optional
+3. Return --- Optional
+*/
+
+
+function mithun(int $num1, int $num2): int{
+   return $num1+$num2;
+}
+
+
+
+$result = mithun('a','b');
+print($result)
+?>
+
